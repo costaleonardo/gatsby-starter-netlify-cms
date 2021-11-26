69c7ae7bbe94192a6c239bbb4ce59fc8fa804662
Leonardo da Costa <costaleonardo97@gmail.com>
2021-11-26 18:31:47 -0500

    Add test page.

---
+++ src/pages/test/index.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+import Layout from "../../components/Layout";
+
+export default class TestPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
